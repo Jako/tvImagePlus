08487644338f03a0797dcd51e85e8f43ee0d1c34
Thomas Jakobi <thomas.jakobi@partout.info>
2016-12-17 02:50:48 +0100

    New translations

--- core/components/imageplus/lexicon/da/default.inc.php
+++ core/components/imageplus/lexicon/da/default.inc.php
@@ -4,8 +4,12 @@ $_lang['imageplus'] = 'Image+';
 $_lang['imageplus.editor_title'] = 'Image+ editor';
 $_lang['imageplus.edit_image'] = 'Rediger billede';
 $_lang['imageplus.alt_text'] = 'Alt-tekst';
+$_lang['imageplus.caption'] = 'Caption';
+$_lang['imageplus.credits'] = 'Credits';
 
 /** Input options render **/
+$_lang['imageplus.section'] = 'Image+ Options';
+$_lang['imageplus.section_desc'] = 'The following options could be overridden by context/system settings. Please read the <a href="http://jako.github.io/ImagePlus/usage/">documentation</a> for the appropriate keys in the context/system settings.';
 $_lang['imageplus.targetwidth'] = 'Ønsket (minimums) bredde';
 $_lang['imageplus.targetwidth_desc'] = 'Den ønskede bredde for det færdige billede';
 $_lang['imageplus.targetheight'] = 'Højde';
@@ -16,6 +20,10 @@ $_lang['imageplus.thumbnailWidth'] = 'Miniature bredde';
 $_lang['imageplus.thumbnailWidth_desc'] = '(Valgfrit, heltal) Bredden på miniaturebilledet i TV-fanen.';
 $_lang['imageplus.allowAltTag'] = 'Alternativ tekst';
 $_lang['imageplus.allowAltTag_desc'] = 'Tillad brugeren at skrive en alternativ (mouse-over) tekst til billedet';
+$_lang['imageplus.allowCaption'] = 'Show Caption Field';
+$_lang['imageplus.allowCaption_desc'] = 'Allow user to enter a caption for the image.';
+$_lang['imageplus.allowCredits'] = 'Show Credits Field';
+$_lang['imageplus.allowCredits_desc'] = 'Allow user to enter a credit for the image.';
 
 /** Output options render **/
 $_lang['imageplus.phpThumbParams'] = 'Ekstra phpThumb parametre';
@@ -45,5 +53,21 @@ $_lang['imageplus.error.image_too_small.msg'] = 'Det valgte billede er for småt
 $_lang['imageplus.error.image_not_found.title'] = 'Billedet blev ikke fundet';
 $_lang['imageplus.error.image_not_found.msg'] = 'Billedet blev ikke fundet og kan dermed ikke blive beskåret. Vælg venligst et andet billede.';
 
+/** System settings **/
+$_lang['area_imageplus'] = 'Image+';
 $_lang['setting_imageplus.debug'] = 'Debug';
 $_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_imageplus.target_width'] = $_lang['imageplus.targetwidth'];
+$_lang['setting_imageplus.target_width_desc'] = $_lang['imageplus.targetwidth_desc'];
+$_lang['setting_imageplus.target_height'] = $_lang['imageplus.targetheight'];
+$_lang['setting_imageplus.target_height_desc'] = $_lang['imageplus.targetheight_desc'];
+$_lang['setting_imageplus.target_ratio'] = $_lang['imageplus.targetRatio'];
+$_lang['setting_imageplus.target_ratio_desc'] = $_lang['imageplus.targetRatio_desc'];
+$_lang['setting_imageplus.thumbnail_width'] = $_lang['imageplus.thumbnailWidth'];
+$_lang['setting_imageplus.thumbnail_width_desc'] = $_lang['imageplus.thumbnailWidth_desc'];
+$_lang['setting_imageplus.allow_alt_tag'] = $_lang['imageplus.allowAltTag'];
+$_lang['setting_imageplus.allow_alt_tag_desc'] = $_lang['imageplus.allowAltTag_desc'];
+$_lang['setting_imageplus.allow_caption'] = $_lang['imageplus.allowCaption'];
+$_lang['setting_imageplus.allow_caption_desc'] = $_lang['imageplus.allowCaption_desc'];
+$_lang['setting_imageplus.allow_credits'] = $_lang['imageplus.allowCredits'];
+$_lang['setting_imageplus.allow_credits_desc'] = $_lang['imageplus.allowCredits_desc'];
