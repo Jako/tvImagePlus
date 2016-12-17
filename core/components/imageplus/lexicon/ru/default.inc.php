437c18196b41cf9edfec1443411b6f578970623f
Thomas Jakobi <thomas.jakobi@partout.info>
2016-12-17 02:50:40 +0100

    New translations

--- core/components/imageplus/lexicon/ru/default.inc.php
+++ core/components/imageplus/lexicon/ru/default.inc.php
@@ -4,8 +4,12 @@ $_lang['imageplus'] = 'Image+';
 $_lang['imageplus.editor_title'] = 'Редактор Image+';
 $_lang['imageplus.edit_image'] = 'Редактировать изображение';
 $_lang['imageplus.alt_text'] = 'Альтернатиный текст (alt)';
+$_lang['imageplus.caption'] = 'Caption';
+$_lang['imageplus.credits'] = 'Credits';
 
 /** Input options render **/
+$_lang['imageplus.section'] = 'Image+ Options';
+$_lang['imageplus.section_desc'] = 'The following options could be overridden by context/system settings. Please read the <a href="http://jako.github.io/ImagePlus/usage/">documentation</a> for the appropriate keys in the context/system settings.';
 $_lang['imageplus.targetwidth'] = 'Необходимая (минимальная) ширина';
 $_lang['imageplus.targetwidth_desc'] = '(Необязательно, Integer) Необходимая ширина для отображаемого изображения. Загружаемое изображение должно иметь эту минимальную ширину.';
 $_lang['imageplus.targetheight'] = 'Необходимая (минимальная) высота';
@@ -16,6 +20,10 @@ $_lang['imageplus.thumbnailWidth'] = 'Ширина иконки';
 $_lang['imageplus.thumbnailWidth_desc'] = '(Необязательно) Ширина изображения в панели управления TV.';
 $_lang['imageplus.allowAltTag'] = 'Показывать поле для атрибута Аlt';
 $_lang['imageplus.allowAltTag_desc'] = 'Позволяет пользователю ввести заголовок/альтернативный текст для изображения';
+$_lang['imageplus.allowCaption'] = 'Show Caption Field';
+$_lang['imageplus.allowCaption_desc'] = 'Allow user to enter a caption for the image.';
+$_lang['imageplus.allowCredits'] = 'Show Credits Field';
+$_lang['imageplus.allowCredits_desc'] = 'Allow user to enter a credit for the image.';
 
 /** Output options render **/
 $_lang['imageplus.phpThumbParams'] = 'Дополнительные параметры phpThumb';
@@ -45,5 +53,21 @@ $_lang['imageplus.error.image_too_small.msg'] = 'Выбранное изобра
 $_lang['imageplus.error.image_not_found.title'] = 'Изображение не найдено';
 $_lang['imageplus.error.image_not_found.msg'] = 'Изображение не найдено и не может быть обрезано. Выберите другое изображение, пожалуйста.';
 
+/** System settings **/
+$_lang['area_imageplus'] = 'Image+';
 $_lang['setting_imageplus.debug'] = 'Debug';
 $_lang['setting_imageplus.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_imageplus.target_width'] = $_lang['imageplus.targetwidth'];
+$_lang['setting_imageplus.target_width_desc'] = $_lang['imageplus.targetwidth_desc'];
+$_lang['setting_imageplus.target_height'] = $_lang['imageplus.targetheight'];
+$_lang['setting_imageplus.target_height_desc'] = $_lang['imageplus.targetheight_desc'];
+$_lang['setting_imageplus.target_ratio'] = $_lang['imageplus.targetRatio'];
+$_lang['setting_imageplus.target_ratio_desc'] = $_lang['imageplus.targetRatio_desc'];
+$_lang['setting_imageplus.thumbnail_width'] = $_lang['imageplus.thumbnailWidth'];
+$_lang['setting_imageplus.thumbnail_width_desc'] = $_lang['imageplus.thumbnailWidth_desc'];
+$_lang['setting_imageplus.allow_alt_tag'] = $_lang['imageplus.allowAltTag'];
+$_lang['setting_imageplus.allow_alt_tag_desc'] = $_lang['imageplus.allowAltTag_desc'];
+$_lang['setting_imageplus.allow_caption'] = $_lang['imageplus.allowCaption'];
+$_lang['setting_imageplus.allow_caption_desc'] = $_lang['imageplus.allowCaption_desc'];
+$_lang['setting_imageplus.allow_credits'] = $_lang['imageplus.allowCredits'];
+$_lang['setting_imageplus.allow_credits_desc'] = $_lang['imageplus.allowCredits_desc'];
